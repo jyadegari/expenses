4b64bb5b60bc6009689053b4bbc0df571a45130a
Javad Yadegari <jyad1866@gmail.com>
2021-04-18 13:39:00 +0430

    create and complete Expense model class

--- src/main/java/jyad/model/Expense.java
+++ src/main/java/jyad/model/Expense.java
@@ -10,25 +10,24 @@ import java.time.LocalDateTime;
 @Data
 public class Expense {
 
-    @ApiModelProperty(example = "89", value = "")
+    @ApiModelProperty(example = "89")
     @JsonProperty("id")
     private Integer id;
 
-    @ApiModelProperty(example = "bus ticket", required = true, value = "")
+    @ApiModelProperty(example = "bus ticket", required = true)
     @JsonProperty("name")
     private String name;
 
-    @ApiModelProperty(example = "gives some more details about expenses", value = "")
+    @ApiModelProperty(example = "gives some more details about expenses")
     @JsonProperty("description")
     private String description;
 
-    @ApiModelProperty(example = "45.5", required = true, value = "")
+    @ApiModelProperty(example = "45.5", required = true)
     @JsonProperty("amount")
     private Double amount;
 
-    @ApiModelProperty(example = "2018-1-2", required = true, value = "")
+    @ApiModelProperty(example = "2018-1-2", required = true)
     @JsonProperty("date")
     private LocalDateTime date;
-
 
 }
